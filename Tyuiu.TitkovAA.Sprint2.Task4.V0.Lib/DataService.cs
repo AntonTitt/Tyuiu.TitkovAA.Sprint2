1ba14795943b59ed344d6d070a88c5e23dc7d537
Антон <vip.tank2005@outlook.com>
2023-10-15 22:05:10 +0500

    Task4

---
+++ Tyuiu.TitkovAA.Sprint2.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TitkovAA.Sprint2.Task4.V0.Lib
+{
+    public class DataService : ISprint2Task4V14
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((x * 3) < (Math.Sqrt(y) + 20) ? Math.Pow((2 + (1) / (x * x)), y) : ((y * y) - (Math.Cos(x) * Math.Cos(x)) + 10) / ((x * x) - (Math.Sin(y) * Math.Sin(y)) + 12), 3);
+        }
+    }
+}
---
+++ Tyuiu.TitkovAA.Sprint2.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.TitkovAA.Sprint2.Task4.V0.Lib;
+
+namespace Tyuiu.TitkovAA.Sprint2.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dt = new DataService();
+            double res = 76.977;
+            Assert.AreEqual(res, dt.Calculate(4, 6));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService dt = new DataService();
+            double res = 72.074;
+            Assert.AreEqual(res, dt.Calculate(5, 6));
+        }
+    }
+}
